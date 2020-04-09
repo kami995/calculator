fc1e2147a8727377ae2cb6cd7f6cc955ac438d86
Kami <mihailopetrovic995@gmail.com>
2020-04-09 18:24:27 +0200

    2

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './style.css';
+
+class App extends React.Component{
+    render(){
+        return(
+            <h1></h1>
+        );
+    }
+}
+
+ReactDOM.render(
+    <App />, document.getElementById('root')
+)

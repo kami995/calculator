954056b11a25daa96b876a534b23aaadc952188d
Kami <mihailopetrovic995@gmail.com>
2020-04-09 20:35:06 +0200

    2

--- src/index.js
+++ src/index.js
@@ -1,15 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './style.css';
+import Number from './number';
+import Display from './display'
 
-class App extends React.Component{
+class Calculator extends React.Component{
     render(){
         return(
-            <h1></h1>
+            <div id="calculator">
+                <Display />
+                <Number id="seven" value="7"></Number>
+                <Number id="eight" value="8"></Number>
+                <Number id="nine" value="9"></Number>
+                <Number id="four" value="4"></Number>
+                <Number id="five" value="5"></Number>
+                <Number id="six" value="6"></Number>
+                <Number id="one" value="1"></Number>
+                <Number id="two" value="2"></Number>
+                <Number id="three" value="3"></Number>
+                <Number id="zero" value="0"></Number>
+                <div id="decimal">.</div>
+                <div id="clear">AC</div>
+                <div id="equals">=</div>
+                <div id="add">+</div>
+                <div id="subtract">-</div>
+                <div id="multiply">*</div>
+                <div id="divide">/</div>
+            </div>
         );
     }
 }
 
 ReactDOM.render(
-    <App />, document.getElementById('root')
+    <Calculator />, document.getElementById('root')
 )

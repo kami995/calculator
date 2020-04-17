c7b84bd4f245256900dbbd806c701f49fc8c9775
Kami <mihailopetrovic995@gmail.com>
2020-04-17 19:50:49 +0200

    Updates

---
+++ precache-manifest.b10e07d7fb10fd1e7fa1a053dbcb28b8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88c440c37bd4f8e7d93f8c19c29ee937",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "8fe8f3a92439fd8fc5df",
+    "url": "/calculator/static/css/main.742cc156.chunk.css"
+  },
+  {
+    "revision": "65bae2385b8208c1e157",
+    "url": "/calculator/static/js/2.a2d1e852.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/calculator/static/js/2.a2d1e852.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8fe8f3a92439fd8fc5df",
+    "url": "/calculator/static/js/main.4d9fb549.chunk.js"
+  },
+  {
+    "revision": "552c96952084d63edc80",
+    "url": "/calculator/static/js/runtime-main.29434a71.js"
+  }
+]);
